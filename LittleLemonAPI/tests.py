618c42816be51a5ce4fedcb7a6af98acf6e43189
Himanshu Alwe <himanshualwe@gmail.com>
2024-03-21 19:54:04 +0530

    test update

--- LittleLemonAPI/tests.py
+++ LittleLemonAPI/tests.py
@@ -5,5 +5,7 @@ class MenuItemTest(TestCase):
     def test_get_item(self):
         item = MenuItem.objects.create(title="IceCream", price=80, inventory=100)
         self.assertEqual(item, "IceCream : 80")
+		self.assertEqual(item.name, 'IceCream')
+        self.assertEqual(item.price, 80)
 
 		
